333afb5d895387e322fb55c46201350091d9c88b
Chewynator76 <Chewynator76@gmail.com>
2026-03-25 14:51:31 -0500

    added a testing functionality and fixed a bug in main.py

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from textnode import TextNode
 from textnode import TextType
 
 def main():
-    text_type_text = TextType("[anchor text](url)")
+    text_type_text = TextType.LINK
     test = TextNode("This is some anchor text", text_type_text, "https://www.boot.dev")
     print(test)
 

2b0e23c794579775fa6a1d3922ac09c98527ecfb
Chewynator76 <Chewynator76@gmail.com>
2026-03-27 11:09:05 -0500

    added some spliting stuff and extracting stuff.

--- src/node_splitter.py
+++ src/node_splitter.py
@@ -1,4 +1,5 @@
 from textnode import TextNode, TextType
+from images_extract import extract_markdown_images, extract_markdown_links
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_list = []
@@ -17,6 +18,75 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                 split_nodes.append(TextNode(sections[i], TextType.TEXT))
             else:
                 split_nodes.append(TextNode(sections[i], text_type))
+        new_list.extend(split_nodes)
+    
+    return new_list
+
+def split_nodes_image(old_nodes):
+    new_list = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_list.append(node)
+            continue
+        split_nodes = []
+        images = extract_markdown_images(node.text)
+
+        if images == []:
+            new_list.append(node)
+            continue
+        text = node.text
+        
+        for i in range(len(images)):
+            delimiter = f"![{images[i][0]}]({images[i][1]})"
+            sections = text.split(delimiter, 1)
+            if len(sections) != 2:
+                raise ValueError("invalid markdown, image section not closed")
+            text = sections[1]
+
+            if sections[0] != "":
+                split_nodes.append(TextNode(sections[0], TextType.TEXT))
+            split_nodes.append(TextNode(images[i][0], TextType.IMAGE, images[i][1]))
+            
+        if text != "":
+            split_nodes.append(TextNode(text, TextType.TEXT))
+
+        new_list.extend(split_nodes)
+    
+    return new_list
+
+
+        
+        
+        
+
+def split_nodes_link(old_nodes):
+    new_list = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_list.append(node)
+            continue
+        split_nodes = []
+        links = extract_markdown_links(node.text)
+
+        if links == []:
+            new_list.append(node)
+            continue
+        text = node.text
+
+        for i in range(len(links)):
+            delimiter = f"[{links[i][0]}]({links[i][1]})"
+            sections = text.split(delimiter, 1)
+            if len(sections) != 2:
+                raise ValueError("invalid markdown, link section not closed")
+            text = sections[1]
+
+            if sections[0] != "":
+                split_nodes.append(TextNode(sections[0], TextType.TEXT))
+            split_nodes.append(TextNode(links[i][0], TextType.LINK, links[i][1]))
+
+        if text != "":
+            split_nodes.append(TextNode(text, TextType.TEXT))
+
         new_list.extend(split_nodes)
     
     return new_list
---
+++ src/test_extracters.py
@@ -0,0 +1,25 @@
+import unittest
+
+from images_extract import extract_markdown_images, extract_markdown_links
+
+class TestHTMLNode(unittest.TestCase):
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links(
+            "This is text with a [link](https://boot.dev) and [another link](https://wikipedia.org)"
+        )
+        self.assertListEqual(
+            [
+                ("link", "https://boot.dev"),
+                ("another link", "https://wikipedia.org"),
+            ],
+            matches,
+        )
+
+if __name__ == "__main__":
+    unittest.main()

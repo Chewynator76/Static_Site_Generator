244ae98c635b0d9cd2f6675b639bc7cbb3eb7bc9
Chewynator76 <Chewynator76@gmail.com>
2026-03-30 13:09:35 -0500

    added a text_to_textnodes function

--- src/node_splitter.py
+++ src/node_splitter.py
@@ -90,3 +90,15 @@ def split_nodes_link(old_nodes):
         new_list.extend(split_nodes)
     
     return new_list
+
+def text_to_textnodes(text):
+    original_text = [TextNode(text, TextType.TEXT)]
+    original_text = split_nodes_delimiter(original_text, "**", TextType.BOLD)
+    original_text = split_nodes_delimiter(original_text, "_", TextType.ITALIC)
+    original_text = split_nodes_delimiter(original_text, "`", TextType.CODE)
+    original_text = split_nodes_image(original_text)
+    original_text = split_nodes_link(original_text)
+    return original_text
+    
+
+    
